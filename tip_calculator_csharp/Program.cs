19d6cb4799dd5529686a58a6fc57c309aa7fff6c
Parker Prewit <nolanprewit1@gmail.com>
2020-07-05 16:04:45 -0500

    added Bill object and method to get bill information

--- tip_calculator_csharp/Program.cs
+++ tip_calculator_csharp/Program.cs
@@ -1,12 +1,69 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks.Dataflow;
 
 namespace tip_calculator_csharp
 {
     class Program
     {
+        public class Bill
+        {
+            public int numerOfPeople { get; set; }
+            public float tipPercentage { get; set; }
+            public float checkTip { get; set; }
+            public float checkTotal { get; set; } 
+            public float checkAmount { get; set; }
+            public float tipPerPerson { get; set; }
+        }
         static void Main()
         {
-            Console.WriteLine("Hello World!");
+            // Create new bill oject
+            var bill = new Bill();
+
+            while (true)
+            {
+                Console.WriteLine("Welcome to Tip Calculator C#");
+                Console.WriteLine("");
+
+                GetBillInformation(bill);               
+            }
+        }
+        public static void GetBillInformation(Bill bill)
+        {
+            Console.Write("Number of people to split the check by [1]:");
+            try
+            {
+                bill.numerOfPeople = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception exception)
+            {
+                if (exception.GetType().Name == "FormatException")
+                {
+                    Console.WriteLine("Please enter a number...");
+                }
+                else
+                {
+                    bill.numerOfPeople = 1;
+                }
+            }
+
+            Console.Write("Check Amount:");
+            try
+            {
+                bill.checkAmount = (float) Convert.ToDecimal(Console.ReadLine());
+            }
+            catch (Exception exception)
+            {
+                if (exception.GetType().Name == "FormatException")
+                {
+                    Console.WriteLine("Please enter a valide check amount...");
+                }
+                else
+                {
+                    bill.numerOfPeople = 1;
+                }
+            }
         }
     }
 }
