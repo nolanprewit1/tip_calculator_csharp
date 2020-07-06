232b43020d1da8a1d518fe7b7be30b7dcdc65936
Parker Prewit <nolanprewit1@gmail.com>
2020-07-05 21:58:06 -0500

    added bill calculation and display

--- tip_calculator_csharp/Program.cs
+++ tip_calculator_csharp/Program.cs
@@ -9,12 +9,79 @@ namespace tip_calculator_csharp
     {
         public class Bill
         {
-            public int numerOfPeople { get; set; }
-            public float tipPercentage { get; set; }
-            public float checkTip { get; set; }
-            public float checkTotal { get; set; } 
-            public float checkAmount { get; set; }
-            public float tipPerPerson { get; set; }
+            public int NumerOfPeople { get; set; }
+            public float CheckAmount { get; set; }
+            public int TipPercentage { get; set; }
+            public float CheckTip { get; set; }
+            public float CheckTotal { get; set; }             
+            public float TipPerPerson { get; set; }
+            public float TotalPerPerson { get; set; }
+        }
+        public static void GetBillInformation(Bill bill)
+        {
+            Console.Write("Number of people to split the check by: ");
+            try
+            {
+                bill.NumerOfPeople = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Please enter a number...");
+                Console.WriteLine("Press any key to restart...");
+                Console.ReadLine();
+                Console.Clear();
+                return;
+            }
+
+            Console.Write("Check Amount: ");
+            try
+            {
+                bill.CheckAmount = float.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Please enter a valide check amount...");
+                Console.WriteLine("Press any key to restart...");
+                Console.ReadLine();
+                Console.Clear();
+                return;
+            }
+
+            Console.Write("Tip %: ");
+            try
+            {
+                bill.TipPercentage = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Please enter a tip percentage...");
+                Console.WriteLine("Press any key to restart...");
+                Console.ReadLine();
+                Console.Clear();
+                return;
+            }
+        }
+
+        public static void CalculateBill(Bill bill)
+        {
+            bill.CheckTip = (bill.CheckAmount / 100) * bill.TipPercentage;
+            bill.CheckTotal = bill.CheckAmount + bill.CheckTip;
+            bill.TipPerPerson = bill.CheckTip / bill.NumerOfPeople;
+            bill.TotalPerPerson = bill.CheckTotal / bill.NumerOfPeople;
+        }
+
+        public static void DisplayBillInformation(Bill bill)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("#### CALCULATED TOTALS ####");
+            Console.Write("Check total: \t\t\t $");
+            Console.WriteLine(Convert.ToString(bill.CheckTotal));
+            Console.Write("Check tip: \t\t\t $");
+            Console.WriteLine(Convert.ToString(bill.CheckTip));
+            Console.Write("Check total per person: \t $");
+            Console.WriteLine(Convert.ToString(bill.TotalPerPerson));
+            Console.Write("Check tip per person: \t\t $");
+            Console.WriteLine(Convert.ToString(bill.TipPerPerson));
         }
         static void Main()
         {
@@ -26,43 +93,9 @@ namespace tip_calculator_csharp
                 Console.WriteLine("Welcome to Tip Calculator C#");
                 Console.WriteLine("");
 
-                GetBillInformation(bill);               
-            }
-        }
-        public static void GetBillInformation(Bill bill)
-        {
-            Console.Write("Number of people to split the check by [1]:");
-            try
-            {
-                bill.numerOfPeople = Convert.ToInt32(Console.ReadLine());
-            }
-            catch (Exception exception)
-            {
-                if (exception.GetType().Name == "FormatException")
-                {
-                    Console.WriteLine("Please enter a number...");
-                }
-                else
-                {
-                    bill.numerOfPeople = 1;
-                }
-            }
-
-            Console.Write("Check Amount:");
-            try
-            {
-                bill.checkAmount = (float) Convert.ToDecimal(Console.ReadLine());
-            }
-            catch (Exception exception)
-            {
-                if (exception.GetType().Name == "FormatException")
-                {
-                    Console.WriteLine("Please enter a valide check amount...");
-                }
-                else
-                {
-                    bill.numerOfPeople = 1;
-                }
+                GetBillInformation(bill);
+                CalculateBill(bill);
+                DisplayBillInformation(bill);
             }
         }
     }
